244bbaef4d1ecf9a9932a362b0dc689c015dbdb0
Jacob <scheuerjacob@gmail.com>
2018-12-04 15:37:43 -0500

    first test get and set start time passed

--- src/main/java/com/pillar/Shift.java
+++ src/main/java/com/pillar/Shift.java
@@ -1,5 +1,14 @@
 package com.pillar;
 
 public class Shift {
+	int startTime;
 
+	public int getStartTime() {
+		return startTime;
+	}
+
+	public void setStartTime(int startTime) {
+		this.startTime = startTime;
+	}
+	
 }
--- src/test/java/com/pillar/test/ShiftTest.java
+++ src/test/java/com/pillar/test/ShiftTest.java
@@ -1,6 +1,14 @@
 package com.pillar.test;
+
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
+import com.pillar.Shift;
 
 public class ShiftTest {
-
+	@Test
+	public void testSetAndGetStartTime() {
+		Shift shift = new Shift();
+		shift.setStartTime(5);
+		assertEquals(5, shift.getStartTime());
+	}
 }
