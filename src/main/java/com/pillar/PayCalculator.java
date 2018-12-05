8143406071bf31d3b6614df7179f2a01933f0277
Jacob <scheuerjacob@gmail.com>
2018-12-05 10:18:58 -0500

    calculates pay for family A when shift spans 2 pay rates

--- src/main/java/com/pillar/PayCalculator.java
+++ src/main/java/com/pillar/PayCalculator.java
@@ -7,8 +7,12 @@ public class PayCalculator {
 	public int calculatePay(Family family, Shift shift) {
 		
 		if(family.getFamilyName().equals("A")) {
-			if(shift.endTime <= 11 && shift.endTime > 5) {
-				this.payOut = family.getRate(0) * shift.calculateTotalHours(); 
+			int hoursBefore11 = 11 - shift.getStartTime();
+			this.payOut += hoursBefore11 * family.getRate(0); 
+			
+			if(shift.getEndTime() == 12 || shift.getEndTime() <= 4) {
+				int hoursAfter11 = shift.calculateTotalHours() - hoursBefore11;
+				this.payOut += hoursAfter11 * family.getRate(1);
 			}
 		}
 		return payOut;
--- src/test/java/com/pillar/test/PayCalculatorTest.java
+++ src/test/java/com/pillar/test/PayCalculatorTest.java
@@ -2,6 +2,7 @@ package com.pillar.test;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import com.pillar.Family;
@@ -9,16 +10,30 @@ import com.pillar.PayCalculator;
 import com.pillar.Shift;
 
 public class PayCalculatorTest {
+	Family family;
+	Shift shift;
+	PayCalculator payCalculator;
+	
+	@Before
+	public void setup() {
+		family = new Family();
+		shift = new Shift();
+		payCalculator = new PayCalculator();
+	}
+	
 	@Test
-	public void testCalculateRateFamilyAOneRateOnly() {
-		Family family = new Family();
-		Shift shift = new Shift();
-		PayCalculator payCalculator = new PayCalculator();
+	public void testCalculatePayForFamilyAOneRateOnly() {
 		family.setFamilyName("A");
 		shift.setStartTime(5);
 		shift.setEndTime(11);
 		assertEquals(90, payCalculator.calculatePay(family, shift));
 	}
-
+	@Test
+	public void testCalculatePayForFamilyABothRates() {
+		family.setFamilyName("A");
+		shift.setStartTime(5);
+		shift.setEndTime(12);
+		assertEquals(110, payCalculator.calculatePay(family, shift));
+	}
 
 }
