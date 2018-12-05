be2f65cdc281a231fd61868f05acd06217b7ea89
Jacob <scheuerjacob@gmail.com>
2018-12-05 13:00:27 -0500

    passed test for calculating first rate for family C

--- src/main/java/com/pillar/FamilyC.java
+++ src/main/java/com/pillar/FamilyC.java
@@ -1,10 +1,16 @@
 package com.pillar;
 
 public class FamilyC implements Payable{
-
+	
+	private int payOut;
+	private int[] rates = new int[] {21, 15};
+	
 	@Override
 	public int calculatePay(Shift shift) {
-		return 84;
+		int hoursBefore9 = 9 - shift.getStartTime();
+		this.payOut += hoursBefore9 * rates[0]; 
+		
+		return this.payOut;
 	}
 
 }
