86a53f31a14a09e6bf8eb2cbb73ab793c4c5b85b
Jacob <scheuerjacob@gmail.com>
2018-12-05 09:42:25 -0500

    created PayCalculator class and ran first test

---
+++ src/main/java/com/pillar/PayCalculator.java
@@ -0,0 +1,9 @@
+package com.pillar;
+
+public class PayCalculator {
+	
+	public int calculatePay(Family family, Shift shift) {
+		return 90;
+	}
+
+}
--- src/test/java/com/pillar/test/PayCalculatorTest.java
+++ src/test/java/com/pillar/test/PayCalculatorTest.java
@@ -1,18 +1,22 @@
 package com.pillar.test;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
+import com.pillar.Family;
+import com.pillar.PayCalculator;
 import com.pillar.Shift;
 
 public class PayCalculatorTest {
-//	@Test
-//	public void testCalculateRateA1() {
-//		Family family = new Family();
-//		Shift shift = new Shift();
-//		PayCalculator payCalculator = new PayCalculator();
-//		shift.setStartTime(5);
-//		shift.setEndTime(11);
-//		assertEquals(90, payCalculator.calculatePay(family, shift));
-//	}
+	@Test
+	public void testCalculateRateA1() {
+		Family family = new Family();
+		Shift shift = new Shift();
+		PayCalculator payCalculator = new PayCalculator();
+		shift.setStartTime(5);
+		shift.setEndTime(11);
+		assertEquals(90, payCalculator.calculatePay(family, shift));
+	}
 
 }
